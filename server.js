a74d6b4d0a2f28a20de606a00d675534952a6113
wanzofc only <zanssxploit@gmail.com>
2025-05-04 07:57:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
 const app = express()
 const PORT = process.env.PORT || 3000
 
